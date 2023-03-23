c5e94998409c05c1fe9fa3ba98b5301fa6a43ee2
Abdikarim Hassan Abdi <79750412+Abdi202@users.noreply.github.com>
2023-03-23 09:55:14 +0000

    LoopArry.java
    Simple logic how work loopArray

---
+++ LoopArry.java
@@ -0,0 +1,21 @@
+package UpdateCouse;
+
+import java.util.Arrays;
+
+public class LoopArryMain {
+
+
+    public static void main(String[]args){
+        String[] countrys ={"Somlai","India","Sodan","Qadar","libia","turkey"};
+        
+        
+
+        for(int i =0; i<countrys.length;i++){
+          
+            System.out.println(i+": "+countrys[i]);
+            System.out.println("\n");
+         
+
+    }
+    }
+}
