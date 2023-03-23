ebf097fddc8e384c08cfde8c9e2821e7220a680c
Abdikarim Hassan Abdi <79750412+Abdi202@users.noreply.github.com>
2023-03-23 09:59:26 +0000

    _2dArray
     Simple  logic how to work_2dArray

---
+++ _2dArray.java
@@ -0,0 +1,46 @@
+package UpdateCouse;
+
+import java.util.Arrays;
+
+public class _2dArray {
+
+    public static void main(String[]args){
+
+        // double[][] Students = new double[3][4];
+
+    //System.out.println(Arrays.toString(Students[2]));
+
+        //  Students[0][0] =74.4;
+        //  Students[0][1] =67.5;
+        // Students[0][2] =89.3;
+        // Students[0][3] =54.76;
+
+        // Students[1][0] =24.4;
+        // Students[1][1] =76.5;
+        // Students[1][2] =86.3;
+        // Students[1][3] =63.76;
+
+        // Students[2][0] =35.4;
+        // Students[2][1] =73.5;
+        // Students[2][2] =93.3;
+        // Students[2][3] =66.76;
+
+        // System.out.println(Arrays.toString(Students[0]));
+        // System.out.println(Arrays.toString(Students[1]));
+        // System.out.println(Arrays.toString(Students[2]));
+
+        // System.out.println(Arrays.toString(Students));
+        // System.out.println(Students[0][0]+" "+Students[0][1]+" "+Students[0][2]+" "+Students[0][3]);
+
+ double [][] Students = {{74.4,67.5,89.3,54.76 },
+     {24.4,76.5,86.3,63.76},
+     {63.76,35.4,73.5,93.3}};
+   // 2d array farst ways  use like that
+// System.out.println(Students[0][0]+" "+Students[0][1]+" "+Students[0][2]+" "+Students[0][3]);
+ System.out.println(Arrays.toString(Students[0]));
+ System.out.println(Arrays.toString(Students[1]));
+ System.out.println(Arrays.toString(Students[2]));
+ 
+    }
+    
+}
